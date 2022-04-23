dce31bc124c94e8e55f4239abb8c804bfdb5841a
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-24 00:19:32 +0600

    header creation, route setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/Pages/About/About";
+import Blogs from "./components/Pages/Blogs/Blogs";
+import Contact from "./components/Pages/Contact/Contact";
+import Home from "./components/Pages/Home/Home";
+import NotFound from "./components/Pages/NotFound/NotFound";
+import Services from "./components/Pages/Services/Services";
+import Header from "./components/Shared/Header/Header";
 
 function App() {
   return (
-    <div>
-      <p className="text-2xl text-indigo-400">Welcome to tailwind css</p>
+    <div className="Full-Website">
+      <div className="Header">
+        <Header></Header>
+      </div>
+      <div className="Body">
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/services" element={<Services></Services>}></Route>
+          <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="*" element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </div>
+      <div className="Footer"></div>
     </div>
   );
 }
---
+++ src/components/Pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Something went wrong 404.</h2>
+    </div>
+  );
+};
+
+export default NotFound;
