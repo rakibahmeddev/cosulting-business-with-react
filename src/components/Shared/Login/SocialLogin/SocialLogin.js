9186bc7a7bb12d685aad597033ecf6a885dfc1ce
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-26 11:16:38 +0600

    github login and sign up added

--- src/components/Shared/Login/SocialLogin/SocialLogin.js
+++ src/components/Shared/Login/SocialLogin/SocialLogin.js
@@ -1,11 +1,15 @@
 import React from "react";
 import google from "../../../../images/socialLogin/google.png";
 import github from "../../../../images/socialLogin/github.png";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSignInWithGoogle,
+  useSignInWithGithub,
+} from "react-firebase-hooks/auth";
 import auth from "../../../../firebase.init";
 
 const SocialLogin = () => {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
+  const [signInWithGithub] = useSignInWithGithub(auth);
   return (
     <div className="social-login">
       {/* google sign in  */}
@@ -26,7 +30,10 @@ const SocialLogin = () => {
 
       {/* Github login  */}
       <div className="my-3">
-        <button className="border bg-black text-white font-medium rounded-lg flex justify-center items-center d-block mx-auto px-5 py-2">
+        <button
+          onClick={() => signInWithGithub()}
+          className="border bg-black text-white font-medium rounded-lg flex justify-center items-center d-block mx-auto px-5 py-2"
+        >
           <img
             style={{ width: "30px", height: "30px" }}
             src={github}
